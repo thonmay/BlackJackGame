284f8a0546fe36ba161dcdd41ae1143648810bb1
Thonmay <mdthoriqulislam384@gmail.com>
2025-03-02 22:38:46 +0100

    Feat: added user interface for the BlackJack game

---
+++ index.js
@@ -0,0 +1,22 @@
+let firstCard = Math.floor(Math.random() * 10 + 2);
+let secondCard = Math.floor(Math.random() * 10 + 2);
+
+let sum = firstCard + secondCard;
+
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+if(sum <= 20) {
+    message = "Do you want to draw a new card?🙂 ";
+} else if (sum === 21) {
+    message = "You've got Blackjack!🤩";
+    hasBlackJack = true;
+} else {
+    message = "You're out of the game!😒";
+    isAlive = false;
+}
+
+console.log(message);
+
+
