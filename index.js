e4c66d6efa6422fc84521a204f5d62170d387e1e
Thonmay <mdthoriqulislam384@gmail.com>
2025-03-03 15:43:41 +0100

    putting everything in order

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-let cards = [];
-let sum;
+//Game State Variables
+let deck = [];
+let playerCards = [];
+let sum = 0;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+
+//Card Suits & Values
+const SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"];
+const VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+
 
 //Elements
 const msgEl = document.getElementById("msgEl");
@@ -15,30 +22,77 @@ const startBtn = document.getElementById("start-btn");
 msgEl.textContent = "Wanna play a game of Black Jack?";
 startBtn.style.visibility = 'visible';
 
-function getRandomCard() {
-    return Math.floor(Math.random() * 10 + 2);   //A card between 2 and 11
+//Creating Deck
+function createDeck() {
+    //Iterating over suits and values
+    let newDeck = [];
+    for(let suit of SUITS)
+    {
+        for(let value of VALUES)
+        {
+            //Creating a Card Object & Inserting it into the Deck
+            const card = {
+                suit: suit,
+                value: (["Jack", "Queen", "King", "Ace"].includes(value)) ? value : 
+                parseInt(value) //The value of the card, assuming Ace to be 10 for now
+            }
+            newDeck.push(card);
+        }
+        return newDeck;
+    }
 }
+
+//Shuffle Deck
+function shuffleDeck(deck) {
+    //Using Fisher-Yates Algorithm
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]]; // Swap elements
+    }
+    return deck;
+}
+
+
+//Get Random Card
+function getRandomCard() {
+    if(deck.length < 10)
+        deck = shuffleDeck(createDeck());
+    return deck.pop();
+}
+
 
 function startGame()
 {
+    resetGame();
     isAlive = true;
     hasBlackJack = false;
-    cards = [getRandomCard(), getRandomCard()];
+    deck = shuffleDeck(createDeck());
+    playerCards = [getRandomCard(), getRandomCard()];
+    console.log(playerCards);
     renderGame();
     startBtn.style.visibility = 'hidden';
+    sumEl.innerHTML = "";
 }
 
 function resetGame()
 {
     cards = [];
+    cardEl.innerHTML = "";
     startBtn.style.visibility = 'visible';
 }
 
 
 function renderGame() {
+
+    cardEl.innerHTML = "";
+    // Display each card as an image
+    playerCards.forEach(card => {
+        const cardImage = renderCardImage(card);
+        cardEl.appendChild(cardImage);
+    });
     
-    sum = cards.reduce( (total, card) => total + card, 0);
-    console.log(cards.join(", "));
+    sum = playerCards.reduce( (total, card) => total + (["Jack", "Queen", "King", "Ace"].includes(card.value) ? 10 : card.value), 0);
+    console.log(playerCards.join(", "));
     if(sum <= 20) {
         message = "Do you want to draw a new card?🙂 ";
     } else if (sum === 21) {
@@ -46,15 +100,16 @@ function renderGame() {
         hasBlackJack = true;
         isAlive = false;
         celebrateWin();
-        resetGame();
+        setTimeout(resetGame, 3000);
+        //resetGame();
     } else {
-        message = "You're out of the game!😒";
+        message = "BUST!!! You're out of the game!😒";
         isAlive = false;
         showFailure();
-        setTimeout(resetGame, 1000);
+        setTimeout(resetGame, 2000);
         //resetGame();
     }
-    cardEl.textContent = "Cards: " + cards.join(" ");
+    //cardEl.textContent = "Cards: " + cards.join(" ");
     sumEl.textContent = "Sum: " + sum;
     msgEl.textContent = message;
 }
@@ -64,10 +119,28 @@ function newCard()
     // Only allow drawing a card if the game is still active
     if (isAlive && !hasBlackJack) {
         let newCard = getRandomCard();
-        cards.push(newCard);
+        playerCards.push(newCard);
         sum += newCard;
         renderGame();
     }
+}
+
+// Render card image
+function renderCardImage(card) {
+    const cardImg = document.createElement('img');
+    
+    let cardName;
+    if (typeof (card.value) === 'string') {
+        cardName = card.value.toLowerCase() + '_of_' + card.suit.toLowerCase();
+    } else {
+        cardName = card.value + '_of_' + card.suit.toLowerCase();
+    }
+    
+    cardImg.src = `images/${cardName}.png`;
+    cardImg.alt = `${card.value} of ${card.suit}`;
+    cardImg.className = 'card-img';
+    
+    return cardImg;
 }
 
 
