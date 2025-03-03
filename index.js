7a6f39a41740284f298e6e948d8a919ba599c8be
Thonmay <mdthoriqulislam384@gmail.com>
2025-03-03 23:53:28 +0100

    Feat: added dealer functionality to simulate the actual game

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //Game State Variables
 let deck = [];
 let playerCards = [];
-let sum = 0;
+let dealerCards = [];
+let playerSum = 0;
+let dealerSum = 0;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
@@ -13,10 +15,16 @@ const VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "
 
 //Elements
 const msgEl = document.getElementById("msgEl");
-const cardEl = document.getElementById("cardEl");
-const sumEl = document.getElementById("sumEl");
+const playersCardEl = document.getElementById("playersCardEl");
+const dealersCardEl = document.getElementById("dealersCardEl");
+const playerSumEl = document.getElementById("playerSumEl");
+const dealerSumEl = document.getElementById("dealerSumEl");
 const gameContainer = document.getElementById("game-container");
 const startBtn = document.getElementById("start-btn");
+const newCardBtn = document.getElementById("new-card-btn");
+const standBtn = document.getElementById("stand-btn");
+const h2El = document.querySelectorAll("h2");
+
 
 //Initial Message
 msgEl.textContent = "Wanna play a game of Black Jack?";
@@ -67,6 +75,7 @@ function startGame()
     isAlive = true;
     hasBlackJack = false;
     deck = shuffleDeck(createDeck());
+    dealerCards = [getRandomCard(), getRandomCard()];
     playerCards = [getRandomCard(), getRandomCard()];
     renderGame();
     startBtn.style.visibility = 'hidden';
@@ -74,50 +83,90 @@ function startGame()
 
 function resetGame()
 {
-    cards = [];
-    cardEl.innerHTML = "";
+    playersCardEl.innerHTML = "";
+    dealersCardEl.innerHTML = "";
     startBtn.style.visibility = 'visible';
-    sumEl.textContent = "";
+    playerSumEl.textContent = "";
+    dealerSumEl.textContent = "";
+    h2El[0].textContent =  "";
+    h2El[1].textContent = "";
+    standBtn.style.visibility = 'hidden';
+    newCardBtn.style.visibility = 'hidden';
+    msgEl.textContent = "Another Round?";
 }
 
 
 function renderGame() {
 
-    cardEl.innerHTML = "";
+    playersCardEl.innerHTML = "";
+    dealersCardEl.innerHTML = "";
     // Display each card as an image
+    dealerCards.forEach(card => {
+        const cardImage = renderCardImage(card);
+        dealersCardEl.appendChild(cardImage);
+    });
     playerCards.forEach(card => {
         const cardImage = renderCardImage(card);
-        cardEl.appendChild(cardImage);
+        playersCardEl.appendChild(cardImage);
     });
     
-    sum = playerCards.reduce( (total, card) => total + (["Jack", "Queen", "King", "Ace"].includes(card.value) ? 10 : card.value), 0);
-    if(sum <= 20) {
+    playerSum = playerCards.reduce( (total, card) => total + (["Jack", "Queen", "King", "Ace"].includes(card.value) ? 10 : card.value), 0);
+    dealerSum = dealerCards.reduce( (total, card) => total + (["Jack", "Queen", "King", "Ace"].includes(card.value) ? 10 : card.value), 0);
+
+    if(playerSum <= 20 && dealerSum <= 20) {
         message = "Do you want to draw a new card?🙂 ";
-    } else if (sum === 21) {
+    } else if (playerSum === 21) {
         message = "You've got Blackjack!🤩";
         hasBlackJack = true;
         isAlive = false;
         celebrateWin();
-        setTimeout(resetGame, 3000);
+        setTimeout(resetGame, 1500);
         //resetGame();
-    } else {
-        message = "BUST!!! You're out of the game!😒";
+    } else if( dealerSum > 21) {
+        message = "Dealer Busts! You Win!😎";
+        isAlive = false;
+        celebrateWin();
+        setTimeout(resetGame, 1500)
+    } else if(dealerSum === 21)
+    {
+        message = "Dealer has Blackjack! You Lose!😒";
         isAlive = false;
         showFailure();
-        setTimeout(resetGame, 2000);
+        setTimeout(resetGame, 1000);
     }
-    //cardEl.textContent = "Cards: " + cards.join(" ");
-    sumEl.textContent = "Sum: " + sum;
+     else {
+        message = "BUST!!! You Lose!😒";
+        isAlive = false;
+        showFailure();
+        setTimeout(resetGame, 1000);
+    }
+    dealerSumEl.textContent = "Sum: " + dealerSum;
+    playerSumEl.textContent = "Sum: " + playerSum;
     msgEl.textContent = message;
+    h2El[0].textContent =  "Dealer Cards";
+    h2El[1].textContent = "Your Cards";
+    standBtn.style.visibility = 'visible';
+    newCardBtn.style.visibility = 'visible';
 }
 
+
+standBtn.addEventListener("click", addDealerCard);
+
+function addDealerCard() {
+    if (isAlive && !hasBlackJack) {
+        let newCard = getRandomCard();
+        dealerCards.push(newCard);
+        dealerSum += newCard;
+        renderGame();
+    } 
+}
 function newCard()
 {
     // Only allow drawing a card if the game is still active
     if (isAlive && !hasBlackJack) {
         let newCard = getRandomCard();
         playerCards.push(newCard);
-        sum += newCard;
+        playerSum += newCard;
         renderGame();
     }
 }
@@ -147,6 +196,11 @@ function celebrateWin() {
     // Change background with transition
     gameContainer.backgroundColor = "#1a9c50"; // Festive green
     console.log("win")
+
+    //Hide all the buttons
+    startBtn.style.visibility = 'hidden';
+    newCardBtn.style.visibility = 'hidden';
+    standBtn.style.visibility = 'hidden';
     
     // Create confetti
     createConfetti(100); // Create 100 confetti pieces
